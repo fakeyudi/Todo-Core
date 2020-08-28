cf3ad853ec5d9656d8566d97c5aa24e40414cb22
YUDI <udixit419@gmail.com>
2020-08-28 19:18:25 +0530

    Initial Commit

---
+++ Todo Core/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todo Core
+//
+//  Created by Utkarsh Dixit on 28/8/20.
+//  Copyright © 2020 Utkarsh Dixit. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
